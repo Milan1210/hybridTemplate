f7b0f99e07d0ca12b2f45fd33a7f1f2dfdc3cc02
milan <stanojevic.m1991@outlook.com>
2022-06-04 18:28:37 +0200

    add project

---
+++ src/main/java/com/gerniks/app/App.java
@@ -0,0 +1,13 @@
+package com.gerniks.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
