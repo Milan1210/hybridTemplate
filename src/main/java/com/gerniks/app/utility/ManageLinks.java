12794643556086a4187271b540ca2ade5d67737a
milan <stanojevic.m1991@outlook.com>
2022-06-25 21:15:56 +0200

    verify-httRequest

--- src/main/java/com/gerniks/app/utility/ManageLinks.java
+++ src/main/java/com/gerniks/app/utility/ManageLinks.java
@@ -1,6 +1,6 @@
 package com.gerniks.app.utility;
 
-public class RedirectionLinks {
+public class LinkManage {
 
     public String redirectURL(String path){
         return new GenericFunctions().fileRead("file","url").concat(path);
